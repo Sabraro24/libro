c338fc17feb52b922bbd6c2081375a063e63208e
sabraro <sabraro95@gmail.com>
2021-11-17 12:12:14 +0100

    1847.03 almacena el número de páginas de los libros

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numeroPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroPaginasLibro)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = numeroPaginasLibro;
     }
     
     public String getAutorLibro(){
@@ -28,6 +30,10 @@ public class Libro {
     
     public String getTituloLibro(){
         return titulo;
+    }
+    
+    public int getNumeroPaginasLibro(){
+        return numeroPaginas;
     }
     
     public void imprimeAutor(){
